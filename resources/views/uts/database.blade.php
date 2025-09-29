87eaa9ea6b919e924702a38380bf3773b40f766d
adelyaharyani <adelyaharyani07@gmail.com>
2025-09-29 14:04:21 +0700

    uts:uts nomor5

--- resources/views/uts/database.blade.php
+++ resources/views/uts/database.blade.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UTS Database</title>
-</head>
-<body>
-    @include('layouts.header')
+@extends('layouts.app')
 
-    <main>
-        <h2>Menu UTS Database</h2>
-        <p>Ini adalah halaman untuk database.</p>
-    </main>
-
-    @include('layouts.footer')
-</body>
-</html>
+@section('content')
+    <h2>Menu UTS Database</h2>
+    <p>Ini adalah halaman untuk database.</p>
+@endsection
